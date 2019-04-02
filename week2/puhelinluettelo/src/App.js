86edbd9313273f279f8a8fd4f3f93aaa501bacbc
Leevi Heino <leeviheino@live.fi>
2019-04-02 15:54:37 +0300

    Ex. 2.9 done

--- week2/puhelinluettelo/src/App.js
+++ week2/puhelinluettelo/src/App.js
@@ -5,10 +5,15 @@ const Person = (props) =>
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '0400-400-500' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Martti Tienari', number: '040-123456' },
+    { name: 'Arto Järvinen', number: '040-123456' },
+    { name: 'Lea Kutvonen', number: '040-123456' }
   ])
+  const [ filtered, setFiltered ] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ newSearch, setNewSearch ] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -35,9 +40,23 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleSearchChange = (event) => {
+    const results = persons.filter(person => person.name.includes(event.target.value))
+    setFiltered(results)
+    setNewSearch(event.target.value)
+  }
+
+  const list = newSearch ? filtered : persons
+
   return (
     <div>
       <h2>Puhelinluettelo</h2>
+      <form>
+        <div>
+          rajaa näytettäviä: <input value={newSearch} onChange={handleSearchChange} />
+        </div>
+      </form>
+      <h3>Lisää uusi henkilö</h3>
       <form onSubmit={addPerson}>
         <div>
           nimi: <input value={newName} onChange={handleNameChange} />
@@ -50,7 +69,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numerot</h2>
-      {persons.map(person => <Person key={person.name} name={person.name} number={person.number} />)}
+      {list.map(person => <Person key={person.name} name={person.name} number={person.number} />)}
     </div>
   )
 
