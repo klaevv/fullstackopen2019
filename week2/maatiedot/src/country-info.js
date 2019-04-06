103572c733ba3a0e09b7a33f7ae20a915bf3d2b8
Leevi Heino <leeviheino@live.fi>
2019-04-06 13:17:36 +0300

    Week 2 part c

--- week2/maatiedot/src/country-info.js
+++ week2/maatiedot/src/country-info.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Language from './language'
 
 const CountryInfo = ({name, capital, population, languages, flag}) => {
-  console.log('flag: ', flag)
+  const [ weather, setWeather] = useState({})
+
+  useEffect(() => {
+    axios.get(`https://api.apixu.com/v1/current.json?key=2776bc6e854f4592a4f100105190604&q=${capital}`)
+    .then(response => {
+      setWeather(response.data)
+    })
+  }, [])
+
   return (
     <div>
       <h2>{name}</h2>
@@ -18,7 +27,20 @@ const CountryInfo = ({name, capital, population, languages, flag}) => {
           </li>
         )}
       </ul>
-      <img alt="flag" src={flag} />
+      <img
+        alt="flag"
+        src={flag}
+        style={{height: 120}}
+      />
+      <h3>Weather in {capital}</h3>
+      <p>temperature: {weather.current && weather.current.temp_c} celsius</p>
+      <img
+        alt="flag"
+        src={weather.current && weather.current.condition.icon}
+        style={{height: 120}}
+      />
+      <p>wind: {weather.current && weather.current.wind_kph} kph</p>
+      <p>wind direction: {weather.current && weather.current.wind_dir}</p>
     </div>
   )
 }
