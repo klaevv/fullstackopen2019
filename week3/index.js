4f370b668ea153e1d80ee998a0070ec3e56848af
Leevi Heino <leeviheino@live.fi>
2019-04-10 09:43:54 +0300

    Week 3 part a

--- week3/index.js
+++ week3/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
-const app = express()
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const morganBody = require('morgan-body')
+
+const app = express()
 
 app.use(bodyParser.json())
+app.use(morgan('tiny'))
+morganBody(app)
 
 let persons = [
   {
