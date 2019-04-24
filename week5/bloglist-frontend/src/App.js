51fc23095c694a845105ae1a48f2b61947a23588
Leevi Heino <leeviheino@live.fi>
2019-04-24 22:47:55 +0300

    Ex. 5.1  done

---
+++ week5/bloglist-frontend/src/App.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState('')
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+      console.log('user: ', user)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch {
+      console.log('logging error with', username, password)
+    }
+  }
+
+  if (user) {
+    return (
+      <div>
+        <h2>blogs</h2>
+        <p>{`${user.name} logged in :)`}</p>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h2>Kirjaudu</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          käyttäjätunnus
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          salasana
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">kirjaudu</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
