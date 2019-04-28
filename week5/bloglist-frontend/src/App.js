2a8a3abb6e3df2ef75ff36b6731dd9fa428c38b0
Leevi Heino <leeviheino@live.fi>
2019-04-28 13:40:24 +0300

    Ex. 5.6* done

--- week5/bloglist-frontend/src/App.js
+++ week5/bloglist-frontend/src/App.js
@@ -152,7 +152,7 @@ const App = () => {
         <p>{`${user.name} logged in :)`}</p>
         <button type="button" onClick={handleLogout}>logout</button>
         {blogs.map(blog =>
-          <Blog key={blog.id} blog={blog} />
+          <Blog key={blog.id} blog={blog} user={user} />
         )}
         {blogForm()}
       </div>
--- week5/bloglist-frontend/src/components/Blog.js
+++ week5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,42 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState, useEffect } from 'react'
+
+const Blog = ({ blog, user }) => {
+  const [fullInfoVisible, setfullInfoVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  if (fullInfoVisible) {
+    return (
+      <div style={blogStyle}>
+      <div onClick={() => setfullInfoVisible(!fullInfoVisible)}>
+        {blog.title}, {blog.author}
+        <div>
+          {blog.url}
+        </div>
+        <div>
+          likes: {blog.likes} <button type="button" onClick={() => console.log('like')}>like</button>
+        </div>
+        <div>
+          added by: {user.username}
+        </div>
+      </div>
+    </div>
+    )
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div onClick={() => setfullInfoVisible(!fullInfoVisible)}>
+        {blog.title}, {blog.author}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
